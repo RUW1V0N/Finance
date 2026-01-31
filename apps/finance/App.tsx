7c3c948bf5e284af10f6f0f308d6f8648d6a948f
Yauheni Ulitsenak <von1we98@gmail.com>
2026-01-31 13:31:01 +0100

    added apps

---
+++ apps/finance/App.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Universal App 🌍</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 24,
+  },
+});
